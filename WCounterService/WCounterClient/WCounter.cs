dc04bf8776b08cebfc0da4a9c53d625c8db985db
Dimensionnnnn <84111957+Dimensionnnnn@users.noreply.github.com>
2022-10-20 01:14:45 +0600

    Add files via upload

---
+++ WCounterService/WCounterClient/WCounter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WCounterClient.WordsCounterClient;
+
+namespace WCounterClient
+{
+    class WCounter
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            string pathToReadText = "..\\test.txt";
+            string text = File.ReadAllText(pathToReadText);
+
+            text = text.ToLower();
+
+            ServiceClient client = new ServiceClient();
+
+            Dictionary<string, int> distinctWords = client.GetDictionary(text);
+
+            string pathToWriteText = "..\\output.txt";
+            File.WriteAllText(pathToWriteText, "");
+
+            StringBuilder str2Write = new StringBuilder();
+
+            foreach (var word in distinctWords)
+                str2Write.Append(word.Key + '\t' + Convert.ToString(word.Value) + '\n');
+
+            File.AppendAllText(pathToWriteText, str2Write.ToString());
+
+            stopwatch.Stop();
+            Console.WriteLine($"Time spent: {stopwatch.ElapsedMilliseconds}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WCounterService/WCounterService/App_Code/Service.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using WCounterLibrary;
+
+public class Service : IService
+{
+	public Dictionary<string, int> GetDictionary(string text)
+    {
+		return Dictionary.GetDictionaryWMThreading(text);
+    }
+}
